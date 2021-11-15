fefce4cff9f96f017614de49d60d237b8dc71bf6
kevin4moe <kevin4moe@gmail.com>
2021-11-15 11:08:48 -0600

    Create MainPage

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,7 +1,28 @@
-<template></template>
+<template>
+  <main class="flex flex-wrap bg-blue-100">
+    <div 
+      v-for="(student, n) in students"
+      class="w-full sm:w-1/2 lg:w-1/3"
+      :key="n"
+    >
+      <card-view-complete :student="student" />
+    </div>
+  </main>
+</template>
 
 <script>
+import students from '@/assets/json/students.json'
+import CardViewComplete from "@/components/CardViewComplete.vue";
+
 export default {
-  
+  components: {
+    CardViewComplete,
+  },
+  setup() {
+
+     return {
+       students,
+     }
+  }
 }
 </script>
