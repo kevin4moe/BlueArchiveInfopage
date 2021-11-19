687c5e29dde2941101f363c012c5a6a90982a1d1
kevin4moe <kevin4moe@gmail.com>
2021-11-19 10:09:48 -0600

    Just Image components

---
+++ src/components/ImageCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <img class="w-full h-auto" :src="require(`@/assets/students/${student.name}.png`)" alt="">
+</template>
+
+<script>
+export default {
+  props: {
+    student: Object,
+  },
+}
+</script>
