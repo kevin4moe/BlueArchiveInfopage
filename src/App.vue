3b759896352fc37b8d619868a15429e94cb810d7
kevin4moe <kevin4moe@gmail.com>
2021-11-20 18:57:57 -0600

    Header and Footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <MainPage />
+  <main-header />
+  <main-page />
+  <main-footer />
 </template>
 
 <script>
 import MainPage from './components/MainPage.vue'
+import MainFooter from './components/MainFooter.vue'
+import MainHeader from './components/MainHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    MainPage
+    MainPage,
+    MainFooter,
+    MainHeader,
   }
 }
 </script>
