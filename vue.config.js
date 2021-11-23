16b9852393a2d39f24c5789f6c332bc031231a82
kevin4moe <kevin4moe@gmail.com>
2021-11-22 20:35:31 -0600

    Fix favicon error

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  publicPath: '/',
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      const argumentos = args;
+      argumentos[0].template = './public/index.html';
+      return argumentos;
+    });
+  }
+};
