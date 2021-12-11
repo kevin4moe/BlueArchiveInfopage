39e3ac761435a136499bd0698a53da69ffff76aa
kevin4moe <kevin4moe@gmail.com>
2021-12-11 16:18:58 -0600

    Create student filter separate file

---
+++ src/assets/js/student-filter.js
@@ -0,0 +1,71 @@
+class StudentFilter {
+  filter = {
+    rarity: [],
+    school: [],
+    role: [],
+    position: [],
+    attack_type: [],
+    armor_type: [],
+    combat_class: [],
+    weapon_type: [],
+    use_cover: [],
+    urban: [],
+    outdoors: [],
+    indoors: [],
+  };
+  noTags = [
+    "Combat Class", "Rarity", "Academy",
+    "Role", "Position", "ATK", "DEF",
+    "Weapon", "User cover?", "Urban",
+    "Outdoors", "Indoors"
+  ];
+  tags = [];
+
+  addTags(tag, value) {
+    const isTag = this.noTags.findIndex(noTag => noTag === value);
+    // Detectar si existe la categoria
+    if (isTag < 0) {
+      if (this.tags.includes(tag)) {
+        this.filter[tag][0] = value;
+      } else {
+        this.tags.push(tag);
+        this.filter[tag].push(value);
+      }
+    } else {
+      const tagIndex = this.tags.findIndex(tg => tg === tag);
+      this.tags.splice(tagIndex, 1);
+      this.filter[tag].splice(0, 1);
+    }
+  }
+
+  getStudentsByTags(students) {
+    if (this.tags.length > 0) {
+      let sg = students;
+      let getStudents = [];
+      let getStudentsIndex = [];
+      this.tags.forEach((tag) => {
+        getStudents = [];
+        getStudentsIndex = [];
+        sg.forEach((student) => {
+          console.warn(`${student[tag]} === ${this.filter[tag][0]}`)
+          if (student[tag] === this.filter[tag][0]) {
+            console.info(`${student[tag]} === ${this.filter[tag][0]}`)
+            getStudents.push(student);
+
+            const gsIndex = students.findIndex(gs => gs.name === student.name)
+            getStudentsIndex.push(gsIndex);
+          }
+        });
+        console.info(getStudents)
+        sg = getStudents;
+      });
+      return getStudentsIndex;
+    } else {
+      console.warn('Tags are empty. Use addTags(tag, value).')
+      const arrayAll = [...Array(students.length).keys()];
+      return arrayAll;
+    }
+  }
+}
+
+export default StudentFilter;
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -37,6 +37,7 @@
 import students from '@/assets/json/students.json'
 import CardViewComplete from "@/components/CardViewComplete.vue";
 import { ref, reactive } from "vue";
+import StudentFilter from "@/assets/js/student-filter.js";
 
 export default {
   components: {
@@ -57,75 +58,6 @@ export default {
       outdoors: ["Outdoors", "S", "A", "B", "C", "D"],
       indoors: ["Indoors", "S", "A", "B", "C", "D"],
     };
-    class StudentFilter {
-      filter = {
-        rarity: [],
-        school: [],
-        role: [],
-        position: [],
-        attack_type: [],
-        armor_type: [],
-        combat_class: [],
-        weapon_type: [],
-        use_cover: [],
-        urban: [],
-        outdoors: [],
-        indoors: [],
-      };
-      noTags = [
-        "Combat Class", "Rarity", "Academy",
-        "Role", "Position", "ATK", "DEF",
-        "Weapon", "User cover?", "Urban",
-        "Outdoors", "Indoors"
-      ];
-      tags = [];
-
-      addTags(tag, value) {
-        const isTag = this.noTags.findIndex(noTag => noTag === value);
-        // Detectar si existe la categoria
-        if (isTag < 0) {
-          if (this.tags.includes(tag)) {
-            this.filter[tag][0] = value;
-          } else {
-            this.tags.push(tag);
-            this.filter[tag].push(value);
-          }
-        } else {
-          const tagIndex = this.tags.findIndex(tg => tg === tag);
-          this.tags.splice(tagIndex, 1);
-          this.filter[tag].splice(0, 1);
-        }
-      }
-
-      getStudentsByTags(students) {
-        if (this.tags.length > 0) {
-          let sg = students;
-          let getStudents = [];
-          let getStudentsIndex = [];
-          this.tags.forEach((tag) => {
-            getStudents = [];
-            getStudentsIndex = [];
-            sg.forEach((student) => {
-              console.warn(`${student[tag]} === ${this.filter[tag][0]}`)
-              if (student[tag] === this.filter[tag][0]) {
-                console.info(`${student[tag]} === ${this.filter[tag][0]}`)
-                getStudents.push(student);
-
-                const gsIndex = students.findIndex(gs => gs.name === student.name)
-                getStudentsIndex.push(gsIndex);
-              }
-            });
-            console.info(getStudents)
-            sg = getStudents;
-          });
-          return getStudentsIndex;
-        } else {
-          console.warn('Tags are empty. Use addTags(tag, value).')
-          const arrayAll = [...Array(students.length).keys()];
-          return arrayAll;
-        }
-      }
-    }
 
     let studentGroupIndex = ref([...Array(15).keys()]);
     function getAllStudents() {
