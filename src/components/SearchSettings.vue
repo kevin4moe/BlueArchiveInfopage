33a371a07a036978b87c4bd83eb581c483b55756
kevin4moe <kevin4moe@gmail.com>
2022-02-20 13:48:47 -0600

    new index structure

--- src/components/SearchSettings.vue
+++ src/components/SearchSettings.vue
@@ -48,9 +48,9 @@
         <select name="role" class="h-7 rounded bg-gray-800 text-center text-white font-semibold">
           <option value="role" @click="role.changeCurrent(false)" @select="role.changeCurrent(false)">ROLE</option>
           <option 
-            v-for="type in role.types"
+            v-for="(type, index) in role.types"
             :value="type" 
-            @click="role.changeCurrent(type)" @select="role.changeCurrent(type)"
+            @click="role.changeCurrent(index)" @select="role.changeCurrent(index)"
             :key="type"
           >
             {{ type.toUpperCase() }}
@@ -62,17 +62,17 @@
         <ol class="flex flex-row justify-center h-7 text-white font-semibold">
           <li
             class="w-1/2 rounded-l text-center"
-            :class="{'bg-red-800':combatClass.striker.active, 'bg-gray-300 text-black':!combatClass.striker.active}"
-            @click="combatClass.striker.active = !combatClass.striker.active"
+            :class="{'bg-red-800':combatClass.striker, 'bg-gray-300 text-black':!combatClass.striker}"
+            @click="combatClass.striker = !combatClass.striker"
           >
-            {{ combatClass.striker.name }}
+            {{ Object.keys(combatClass)[0].toUpperCase() }}
           </li>
           <li
             class="w-1/2 rounded-r text-center"
-            :class="{'bg-blue-800':combatClass.special.active, 'bg-gray-200 text-black':!combatClass.special.active}"
-            @click="combatClass.special.active = !combatClass.special.active"
+            :class="{'bg-blue-800':combatClass.special, 'bg-gray-200 text-black':!combatClass.special}"
+            @click="combatClass.special = !combatClass.special"
           >
-            {{ combatClass.special.name }}
+            {{ Object.keys(combatClass)[0].toUpperCase() }}
           </li>
         </ol>
         <button
@@ -86,8 +86,8 @@
       <!-- Weapon Type -->
       <div class="flex flex-col justify-evenly w-40">
         <select name="weapon_type" class="h-full m-1 rounded bg-gray-200 text-center text-2xl font-semibold">
-          <option value="Any" @click="weapons.current = false" @select="weapons.current = false">Cualquiera</option>
-          <option v-for="weapon in weapons.type" :value="weapon" :key="weapon" @click="weapons.current = weapon" @select="weapons.current = weapon">
+          <option value="Any" @click="weapons.current = false" @select="weapons.current = false">Weapon</option>
+          <option v-for="(weapon, index) in weapons.types" :value="weapon" :key="weapon" @click="weapons.current = index" @select="weapons.current = index">
             {{ weapon }}
           </option>
         </select>
@@ -111,9 +111,9 @@
         <select name="school" class="h-full m-1 rounded bg-gray-200 text-center text-2xl font-semibold">
           <option value="school" @click="school.changeCurrent(false)" @select="school.changeCurrent(false)">SCHOOL</option>
           <option 
-            v-for="type in school.types"
-            :value="type" 
-            @click="school.changeCurrent(type)" @select="school.changeCurrent(type)"
+            v-for="(type, index) in school.types"
+            :value="type"
+            @click="school.changeCurrent(index)" @select="school.changeCurrent(index)"
             :key="type"
           >
             {{ type.toUpperCase() }}
@@ -132,16 +132,16 @@
             />
           </li>
         </ol>
-        <ol class="flex flex-row justify-evenly mb-1" @click="rarity.change">
-          <li class="flex flex-row items-center text-xl" v-show="rarity.show === 0">
-            ALL&nbsp;
-            <img src="@/assets/icons/star.png" alt="stars" />
-          </li>
-          <li class="flex flex-row items-center text-xl" v-show="rarity.show === -1">
+        <ol class="flex flex-row justify-evenly mb-1" @click="rarity.changeCurrent">
+          <li class="flex flex-row items-center text-xl" v-show="rarity.current === -1">
             ANY&nbsp;
             <img src="@/assets/icons/star.png" alt="stars" />
           </li>
-          <li v-for="stars in 3" v-show="stars <= rarity.show" :key="stars">
+          <li class="flex flex-row items-center text-xl" v-show="rarity.current === 0">
+            ALL&nbsp;
+            <img src="@/assets/icons/star.png" alt="stars" />
+          </li>
+          <li v-for="stars in 3" v-show="stars <= rarity.current" :key="stars">
             <img 
               src="@/assets/icons/star.png"
               alt="stars"
@@ -159,38 +159,23 @@ import { ref, reactive, watch } from "vue";
 export default {
   emits: ["addTags", "newGroup"],
   setup(){
-    const allFilters = {
-      //combat_class: ["Combat Class", "Striker", "Special"],
-      //rarity: ["Rarity", 1, 2, 3],
-      //school: ["Academy","Abydos","Trinity","Gehenna","Millennium","Red Winter","Valkyrie","Hyakkiyako","Shanhaijing"],
-      //role: ["Role","Attacker","Supporter","Tank","Healer"],
-      //position: ["Position","Front","Middle","Back"],
-      //attack_type: ["ATK", "Penetration", "Explosive", "Mystic"],
-      //armor_type: ["DEF", "Heavy", "Light", "Special"],
-      //weapon_type: ["Weapon","HG","SMG","AR","SR","SG","MG","GL","RG","RF","RL","DualSMG","DualMG","MountMG"],
-      //use_cover: ["Use cover?", true, false],
-      //urban: ["Urban", "S", "A", "B", "C", "D"],
-      //outdoors: ["Outdoors", "S", "A", "B", "C", "D"],
-      //indoors: ["Indoors", "S", "A", "B", "C", "D"],
-    };
-
     const rarity = reactive({
-      show: -1,
-      change: () => {
-        if (rarity.show < 3) {
-          rarity.show = ++rarity.show;
+      current: -1,
+      changeCurrent: () => {
+        if (rarity.current < 3) {
+          rarity.current = ++rarity.current;
         } else {
-          rarity.show = -1;
+          rarity.current = -1;
         }
       }
     });
     const combatClass = reactive({
-      striker: {name: "Striker", active: true},
-      special: {name: "Special", active: true}
+      striker: true,
+      special: true
     });
     const useCover = ref(true);
     const weapons = reactive({
-      type: ["HG","SMG","AR","SR","SG","MG","GL","RG","RF","RL","DualSMG","DualMG","MountMG"],
+      types: ["HG","SMG","AR","SR","SG","MG","GL","RG","RF","RL","DualSMG","DualMG","MountMG"],
       current: false
     });
     const school = reactive({
@@ -261,45 +246,47 @@ export default {
       }
     );
     const position = reactive({
-      types: ["all", "front", "middle", "back"],
+      types: ["any", "all", "front", "middle", "back"],
       sortBy: false,
-    });
-    watch(
-      () => position.sortBy,
-      (sortBy) => {
-        sort[0] = "position";
-        sort[1] = sortBy;
+      current: false,
+      changeCurrent: (type) => {
+        if (type === 0) {
+          position.sortBy = false;
+          position.current = false;
+        } else if (type === 1) {
+          position.sortBy = type;
+          position.current = false;
+
+          sort[0] = "position";
+          sort[1] = 0;
+        } else {
+          position.sortBy = false;
+          position.current = type - 2;
+        }
       }
-    );
+    });
 
 
     let searchFilters = {
-      combat_class: false,
-      rarity: false,
-      school: false,
-      role: false,
-      position: false,
-      attack_type: false,
-      armor_type: false,
-      weapon_type: false,
-      use_cover: false,
-      urban: false,
-      outdoors: false,
-      indoors: false,
+      combat_class: Object(),
+      rarity: Number(),
+      school: Number(),
+      role: Number(),
+      attack_type: Number(),
+      armor_type: Number(),
+      weapon_type: Number(),
+      use_cover: Boolean(),
+      sort_by: Array(),
     };
 
     function updateSearchFilters() {
       searchFilters = {
-        combat_class: {
-          striker: combatClass.striker.active,
-          special: combatClass.special.active,
-        },
-        rarity: rarity.show,
-        school: false,
-        role: false,
-        position: position.sortBy,
-        attack_type: false,
-        armor_type: false,
+        combat_class: combatClass,
+        rarity: rarity.current,
+        school: school.current,
+        role: role.current,
+        attack_type: attack_type.current,
+        armor_type: armor_type.current,
         weapon_type: weapons.current,
         use_cover: useCover.value,
         sort_by: sort,
@@ -309,7 +296,6 @@ export default {
 
     return {
       updateSearchFilters,
-      allFilters,
       locations,
       rarity,
       combatClass,
