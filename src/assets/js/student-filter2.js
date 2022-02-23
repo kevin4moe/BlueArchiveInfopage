1b058a782e0ba6a583233ad5a2411acf0ffb4225
kevin4moe <kevin4moe@gmail.com>
2022-02-22 23:13:33 -0600

    rarity view

--- src/assets/js/student-filter2.js
+++ src/assets/js/student-filter2.js
@@ -21,10 +21,11 @@ class StudentFilter {
     sortBy = {
         types: ["rarity"],
         rarity: {
-            1: [4,6,12,22,25,26,31,34,35,39,43,52,59,63],
-            2: [0,1,2,5,7,9,10,11,14,21,24,28,30,33,40,42,44,47,50,55,65,71],
-            3: [3,8,13,15,16,17,18,19,20,23,27,29,32,36,37,38,41,45,46,48,49,51,53,54,56,57,58,60,61,62,64,66,67,68,69,70,72,73],
-        }
+            1: [],
+            2: [],
+            3: [],
+        },
+        active: false
     }
 
     searchStudents(students, state) {
@@ -52,23 +53,30 @@ class StudentFilter {
             });
         }
 
-        if (state.rarity !== 0) {
+        if (state.rarity > -1) {
             this.searchRarity(students, state);
+        } else {
+            this.sortBy.active = false;
         }
     }
 
     searchRarity(students, state) {
         let studentsIndex = [];
+        this.sortBy.rarity[1] = [];
+        this.sortBy.rarity[2] = [];
+        this.sortBy.rarity[3] = [];
         this.studentsIndex.current.forEach((index) => {
             if (students[index].rarity === state.rarity) {
                 studentsIndex.push(index);
-            } else if (state.rarity === -2) {
-                // Maybe some day
+            } else if (state.rarity === 0) {
                 this.sortBy.rarity[students[index].rarity].push(index)
+                this.sortBy.active = true;
             }
         });
         if (state.rarity >  0) {
             this.studentsIndex.current = studentsIndex;
+        } else {
+            this.studentsIndex.current = [...this.sortBy.rarity[3], ...this.sortBy.rarity[2], ...this.sortBy.rarity[1]];
         }
     }
 }
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="flex flex-col min-h-screen bg-blue-100">
     <search-settings @newGroup="newStudentsGroup" />
-    <section class="flex flex-row flex-wrap justify-center">
+    <section v-if="!studentFilter.sortBy.active" class="flex flex-row flex-wrap justify-center">
       <div 
         v-for="index in studentFilter.studentsIndex.current"
         class="flex flex-row flex-wrap"
@@ -11,6 +11,56 @@
       </div>
       <div v-if="studentFilter.studentsIndex.current.length < 74" class="w-full">
         <p class="my-4 text-center"><button class="py-2 px-4 rounded bg-white" @click="getAllStudents">See all</button></p>
+      </div>
+    </section>
+    <section 
+      v-else-if="studentFilter.sortBy.active"
+      class="grid grid-rows-1 gap-4 max-w-screen-lg mx-auto my-12"
+    >
+      <div v-show="studentFilter.sortBy.rarity[3].length > 0" class="flex flex-row">
+        <div class="flex-none flex flex-col justify-center items-center w-12 h-full p-1 rounded bg-yellow-300">
+          <img src="@/assets/icons/star.png" alt="star">
+          <img src="@/assets/icons/star.png" alt="star">
+          <img src="@/assets/icons/star.png" alt="star">
+        </div>
+        <div class="flex flex-row flex-wrap">
+          <div 
+            v-for="index in studentFilter.sortBy.rarity[3]"
+            
+            :key="students[index].name"
+          >
+            <component :is="currentComponent" :student="students[index]" />
+          </div>
+        </div>
+      </div>
+      <div v-show="studentFilter.sortBy.rarity[2].length > 0" class="flex flex-row">
+        <div class="flex-none flex flex-col justify-center items-center w-12 h-full p-1 rounded bg-yellow-300">
+          <img src="@/assets/icons/star.png" alt="star">
+          <img src="@/assets/icons/star.png" alt="star">
+        </div>
+        <div class="flex flex-row flex-wrap">
+          <div 
+            v-for="index in studentFilter.sortBy.rarity[2]"
+            class="flex flex-row flex-wrap"
+            :key="students[index].name"
+          >
+            <component :is="currentComponent" :student="students[index]" />
+          </div>
+        </div>
+      </div>
+      <div v-show="studentFilter.sortBy.rarity[1].length > 0" class="flex flex-row">
+        <div class="flex-none flex flex-col justify-center items-center w-12 h-full p-1 rounded bg-yellow-300">
+          <img src="@/assets/icons/star.png" alt="star">
+        </div>
+        <div class="flex flex-row flex-wrap">
+          <div 
+            v-for="index in studentFilter.sortBy.rarity[1]"
+            class="flex flex-row flex-wrap"
+            :key="students[index].name"
+          >
+            <component :is="currentComponent" :student="students[index]" />
+          </div>
+        </div>
       </div>
     </section>
   </main>
